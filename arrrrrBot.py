f8ac93839a4ebfdc6f355be49173ac49b05aa582
Max Wagner <max.wagner01@gmail.com>
2012-10-21 00:11:01 +0200

    Make tab/space choice consistent

--- arrrrrBot.py
+++ arrrrrBot.py
@@ -1,6 +1,7 @@
 from errbot import BotPlugin, botcmd
 
 class ArrrrrBot(BotPlugin):
-    @botcmd
-    def grog(self, mess, args):
+	@botcmd
+	def grog(self, mess, args):
+		"""Cheers!"""
 		return "GROG GROG GROG"
