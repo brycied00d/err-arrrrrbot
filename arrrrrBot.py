714202ff53146af9e32f5d4380fb92def73a6e91
Sebastian Ullrich <sebasti@nullri.ch>
2012-10-21 17:54:50 +0200

    ʘ‿ʘ

--- arrrrrBot.py
+++ arrrrrBot.py
@@ -13,6 +13,11 @@ class ArrrrrBot(BotPlugin):
 		return "ಠ_ಠ"
 
 	@botcmd
+	def loa(self, mess, args):
+		"""Print the look of approval"""
+		return "ʘ‿ʘ"
+
+	@botcmd
 	def slap(self, mess, args):
 		"""Let the bot slap people in the chatroom in an piratelike manner"""
 		return "{0} slaps {1} around a bit with a large trout.".format(get_sender_username(mess), args)
