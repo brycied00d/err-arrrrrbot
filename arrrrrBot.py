969fbcf119268b934c716557bfaec68d3c37a31a
Sebastian Ullrich <sebasti@nullri.ch>
2012-10-25 14:04:25 +0200

    Further fix: Unicode concatenation in !peekpoke

--- arrrrrBot.py
+++ arrrrrBot.py
@@ -51,9 +51,9 @@ class ArrrrrBot(BotPlugin):
 	@botcmd
 	def peekpoke(self, mess, args):
 		"""List all saved one-liners (private message only)"""
-		result = ''
+		result = u''
 		for name in self.keys():
-			result += "{0}: \t {1}\n".format(name, self[name])
+			result += u"{0}: \t {1}\n".format(name, self[name])
 		return result
 
 	@botcmd(admin_only = True)
